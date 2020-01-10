8d1230e05e3c1f9b728b81ea06eb5a9d67162d4b
Fabian Lozano <fabian.lozanoc23@gmail.com>
2020-01-10 10:39:20 -0800

    Initial commit (first complete version)

---
+++ crawler.py
@@ -0,0 +1,94 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from win10toast import ToastNotifier
+
+import datetime
+import re
+import requests
+from bs4 import BeautifulSoup
+
+from globals import URL, AFFECTED_IMG_SRC_BASE, USERNAME, PASSWORD, EMAILS
+
+"""
+Crawl Forestry website
+"""
+today = datetime.datetime.now()
+output_file = "./crawl-logs/{0}-{1}-{2}_{3}-{4}_affected-profiles.txt".format(today.year, today.month, ("0" + str(today.day)) if today.day < 10 else today.day, today.hour, ("0" + str(today.minute)) if today.minute < 10 else today.minute)
+error_file = "./error-logs/{0}-{1}-{2}_{3}-{4}_errors.txt".format(today.year, today.month, ("0" + str(today.day)) if today.day < 10 else today.day, today.hour, ("0" + str(today.minute)) if today.minute < 10 else today.minute)
+
+# request the html from the page and parse it
+req = requests.get(URL)
+soup = BeautifulSoup(req.text, "lxml")
+
+# create empty list with affected profiles
+affectedProfiles = []
+
+# get all divs with id that starts with "post"
+content = soup.find(id="content")
+profileList = content.find_all(id=re.compile("^post"))
+
+for profile in profileList:
+    try:
+        if AFFECTED_IMG_SRC_BASE in profile.find("img")['src']:
+            
+            # ignore the original profile
+            if profile.find(id="last") and profile.find(id="last").string == "Innes":
+                continue
+
+            affectedProfiles.append(profile)
+    except Exception as e:
+        crawlErrorToast = ToastNotifier()
+        crawlErrorToast.show_toast("Crawl Error", "Could not successfully crawl the profiles page.", duration=5)
+
+# check if there are any bugged profiles 
+if len(affectedProfiles) is not 0:
+    toaster = ToastNotifier()
+    toaster.show_toast("Profile Crawler", "The crawler found affected profiles!", duration=5)
+
+    with open(output_file, "w") as affected_profiles_text:
+        
+        affected_profiles_text.write("The crawler has found the following profiles were affected: \n")
+
+        for affProf in affectedProfiles:
+            # get name strings
+            given = affProf.find(id="first").string if affProf.find(id="first") else ""
+            middle = affProf.find(id="middle").string if affProf.find(id="middle") else ""
+            last = affProf.find(id="last").string if affProf.find(id="last") else "" 
+
+            # add spaces to given name and middle initial conditionally
+            given += " "
+            middle = middle + " " if len(middle) is not 0 else middle
+
+            full_name = given + middle + last
+            affected_profiles_text.write(full_name + "\n")
+    
+    try:
+        """
+        Set up SMTP server
+        """
+        s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
+        s.starttls()
+        s.login(USERNAME, PASSWORD)
+
+        for email in EMAILS:
+            msg = MIMEMultipart()
+
+            with open(output_file) as aff_prof_txt:
+                message = aff_prof_txt.read()
+            
+            msg['From'] = USERNAME
+            msg['To'] = email
+            msg['Subject'] = "Forestry Profiles Crawl Alert"
+
+            msg.attach(MIMEText(message, 'plain'))
+
+            s.send_message(msg)
+
+            del msg
+    except Exception as e:
+        errorToaster = ToastNotifier()
+        errorToaster.show_toast("Profile Crawler Error", "The crawler was unable to send an email report. Please check the errors file for more details", duration=10)
+        
+        with open(error_file, "w") as errors:
+            errors.write(repr(e))
